70af3daa947d936301636d6f14021e4ca966ed36
Malith Senaweera <malithsenaweera@gmail.com>
2017-03-13 12:13:29 +0530

    Integrate auth0

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
-var app = angular.module('RoundRobin', ['ui.router'])
+var app = angular.module('RoundRobin', ['ui.router', 'auth0.auth0', 'angular-jwt'])
 
-app.config(function($stateProvider, $urlRouterProvider){
+app.config(function($stateProvider, $urlRouterProvider, angularAuth0Provider){
 	$stateProvider.state('story', {
 		url: '/story',
 		templateUrl: '../templates/story.html',
@@ -14,4 +14,14 @@ app.config(function($stateProvider, $urlRouterProvider){
 	})
 
 	$urlRouterProvider.otherwise('/signin');
+	angularAuth0Provider.init({
+	  clientID: 'A02UxmKAHdGacjvm4S777a443cQOgjuD',
+	  domain: 'malithsen.auth0.com'
+	});
+});
+
+app.run(function($rootScope, UserService) {
+
+  $rootScope.UserService = UserService;
+  UserService.checkAuthOnRefresh();
 });
--- public/js/services/UserService.js
+++ public/js/services/UserService.js
@@ -1,27 +1,56 @@
 angular.module('RoundRobin')
-.factory('UserService', function($http, $q, $state){
+.factory('UserService', function($http, $q, $state, authManager, angularAuth0, jwtHelper, $location){
 	var baseUrl = "http://127.0.0.1:3000";
 	var authenticated = false;
 	var user = { nickname: "" };
 
 	var o = {};
 
-	o.signin = function(writer){
+	function onAuthenticated(error, authResult) {
+    if (error) {
+      console.log("login failed");
+    }
 
-		$http.post('/user/signin', writer).then(function(response){
-			$state.go('story');
-			authenticated = true;
-			user = response.data;
-		}, function(err){
-			console.log(err);
-			authenticated = false;
-		});
+    localStorage.setItem('id_token', authResult.idToken);
+    authManager.authenticate();
+
+    angularAuth0.getProfile(authResult.idToken, function (error, profileData) {
+      if (error) {
+        return console.log(error);
+      }
+
+      localStorage.setItem('profile', JSON.stringify(profileData));
+      userProfile = profileData;
+
+      $location.path('/story');
+    });
+  }
+
+	o.signin = function(){
+		angularAuth0.login({
+      connection: 'google-oauth2',
+      responseType: 'token',
+      popup: true
+    }, onAuthenticated);
 	}
 
 	o.signout = function(){
-		authenticated =  false;
+		localStorage.removeItem('id_token');
+    localStorage.removeItem('profile');
 		user = { nickname: "" };
+    authManager.unauthenticate();
 	}
+
+  o.checkAuthOnRefresh = function() {
+    var token = localStorage.getItem('id_token');
+    if (token) {
+      if (!jwtHelper.isTokenExpired(token)) {
+        authManager.authenticate();
+      }
+    } else {
+      $location.path('/signin');
+    }
+  }
 
 	o.isAuthenticated = function(){
 		return authenticated;
