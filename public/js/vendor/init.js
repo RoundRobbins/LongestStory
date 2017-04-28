ccfc640700f2faa8d76f81944f3415d0b41b1e85
Malith Senaweera <malithsenaweera@gmail.com>
2017-04-28 23:37:20 +0530

    Integrate new UI. WIP

---
+++ public/js/vendor/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    var scene = document.getElementById('scene');
+    var parallax = new Parallax(scene);
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
